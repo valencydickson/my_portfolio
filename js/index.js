57caf9709d60f635df3e5be1c031bd186cb7143a
valencydickson <dickson.valency@gmail.com>
2021-12-16 10:40:47 +0300

    remove unused js

--- js/index.js
+++ js/index.js
@@ -30,52 +30,6 @@ $(window).ready(function () {
     previousScroll = scrollTop;
   });
 
-  //AnimateJs
-
-
-
-  //TRIGGERING ANIMATION WHEN SCROLLING
-
-  // get the element to animate
-  var element = document.querySelector(".skills");
-  var elementHeight = element.clientHeight;
-
-  // listen for scroll event and call animate function
-  document.addEventListener("scroll", animate);
-
-  // check if element is in view
-  function inView() {
-    // get window height
-    var windowHeight = window.innerHeight + 150; //150px increase the viewport of element
-    // get number of pixels that the document is scrolled
-    var scrollY = window.scrollY || window.pageYOffset;
-
-    // get current scroll position (distance from the top of the page to the bottom of the current viewport)
-    var scrollPosition = scrollY + windowHeight;
-    // get element position (distance from the top of the page to the bottom of the element)
-    var elementPosition =
-      element.getBoundingClientRect().top + scrollY + elementHeight;
-
-    // is scroll position greater than element position? (is element in view?)
-    if (scrollPosition > elementPosition) {
-      return true;
-    }
-
-    return false;
-  }
-
-  // animate element when it is in view
-  function animate() {
-    // is element in view?
-    if (inView()) {
-      // element is in view, add class to elements
-      var lines = document.querySelectorAll(".line");
-      var linesArray = Array.from(lines);
-      linesArray.forEach(function (line) {
-        line.classList.add("animate");
-      });
-    }
-  }
 
 
 
